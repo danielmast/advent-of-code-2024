53b72d37c40c2682cc536bd4176f2ddf408bce72
danielmast <danielmast92@gmail.com>
2024-12-20 20:03:08 +0100

    Day 20

---
+++ day20/day20_2.py
@@ -0,0 +1,99 @@
+from collections import defaultdict
+
+
+def main():
+    map, start, end = read_file('input.txt')
+
+    route = get_route(map, start, end)
+    cheats = get_cheats(route, map)
+
+    print_cheats(cheats)
+
+    answer = at_least_100(cheats)
+    print(f'The answer: {answer}')
+
+
+def print_cheats(cheats):
+    for saving_time in sorted(cheats.keys()):
+        print(f'There are {len(cheats[saving_time])} cheats that save {saving_time} picoseconds.')
+    print()
+
+
+def at_least_100(cheats):
+    count = 0
+    for saving_time, cs in cheats.items():
+        if saving_time >= 100:
+            count += len(cs)
+    return count
+
+
+def get_cheats(route, map):
+    cheats = defaultdict(set)
+    for i in range(len(route) - 1):
+        for neighbour, saving_time in get_cheat_neighbours(i, route, map):
+            cheats[saving_time].add((route[i], neighbour))
+    return cheats
+
+
+def get_cheat_neighbours(i, route, map):
+    neighbours = []
+
+    pos = route[i]
+    y, x = pos
+    for dy in range(-20, 21, 1):
+        for dx in range(-20, 21, 1):
+            if abs(dy) + abs(dx) <= 20:
+                if 0 < y + dy < len(map) and 0 < x + dx < len(map[0]):
+                    if map[y + dy][x + dx] in ['.', 'E']:
+                        neighbour = (y + dy, x + dx)
+                        j = route.index(neighbour)
+                        saving_time = j - i - abs(dy) - abs(dx)
+                        if j > i:
+                            neighbours.append((neighbour, saving_time))
+
+    return neighbours
+
+
+def get_route(map, start, end):
+    route = [start]
+
+    while route[-1] != end:
+        for neighbour in get_neighbours(route[-1], map):
+            if len(route) == 1 or neighbour != route[-2]:
+                route.append(neighbour)
+                break
+
+    return route
+
+
+def get_neighbours(pos, map):
+    neighbours = []
+
+    y, x = pos
+    for dy in [-1, 0, 1]:
+        for dx in [-1, 0, 1]:
+            if abs(dy) + abs(dx) == 1:
+                if map[y + dy][x + dx] in ['.', 'E']:
+                    neighbours.append((y + dy, x + dx))
+
+    return neighbours
+
+
+def read_file(input_file):
+    input = open(input_file, 'r')
+    lines = input.readlines()
+
+    start = end = None
+    map = []
+    for y, line in enumerate(lines):
+        map.append(line.strip())
+        if 'S' in line:
+            start = (y, line.index('S'))
+        elif 'E' in line:
+            end = (y, line.index('E'))
+
+    return map, start, end
+
+
+if __name__ == "__main__":
+    main()
